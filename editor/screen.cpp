66c38a35ebdbdd1e3ce53ec8eddf723030b93074
Johan Kotlinski <kotlinski@gmail.com>
2014-10-23 18:03:12 +0200

    Added border

--- editor/screen.cpp
+++ editor/screen.cpp
@@ -8,13 +8,17 @@
 
 #include "vicpalette.h"
 
+const int borderMargin = 64;
+static int bgColor = 1;
+static int borderColor = 2;
+
 Screen::Screen(QWidget *parent) :
     QWidget(parent)
 {
 }
 
-static void draw(QPainter *painter, int column, int row, int bgColorIndex, int fgColorIndex, int ch) {
-    Q_ASSERT(bgColorIndex >= 0 && bgColorIndex < 16);
+static void draw(QPainter *painter, int column, int row, int fgColorIndex, int ch) {
+    Q_ASSERT(bgColor >= 0 && bgColor < 16);
     Q_ASSERT(fgColorIndex >= 0 && fgColorIndex < 16);
     static QByteArray charrom;
     if (charrom.isEmpty()) {
@@ -23,7 +27,7 @@ static void draw(QPainter *painter, int column, int row, int bgColorIndex, int f
         charrom = f.readAll();
         Q_ASSERT(charrom.size() == 2 * 256 * 8);
     }
-    QColor bg(vicPalette[bgColorIndex]);
+    QColor bg(vicPalette[bgColor]);
     QColor fg(vicPalette[fgColorIndex]);
     for (int y = row * 8; y < row * 8 + 8; ++y) {
         const char romchar = charrom.at(ch * 8 + y % 8);
@@ -38,16 +42,18 @@ static void draw(QPainter *painter, int column, int row, int bgColorIndex, int f
 void Screen::paintEvent(QPaintEvent *event) {
     (void)event;
     QPainter painter(this);
-    painter.scale(width() / 320.f, height() / 200.f);
+    painter.fillRect(0, 0, width(), height(), vicPalette[borderColor]);
+    painter.translate(borderMargin, borderMargin);
+    painter.scale((width() - borderMargin * 2) / 320.f, (height() - borderMargin * 2) / 200.f);
 
     int ch = 0;
     for (int y = 0; y < 10; ++y) {
         for (int x = 0; x < 40; ++x) {
-            draw(&painter, x, y, 2, 3, ch++);
+            draw(&painter, x, y, 3, ch++);
         }
     }
 }
 
 QSize Screen::minimumSizeHint() const {
-    return QSize(640, 400);
+    return QSize(640 + borderMargin * 2, 400 + borderMargin * 2);
 }
