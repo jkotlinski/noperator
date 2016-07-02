fc7f3a4640856a3b41361a78a072ad79854a794f
Johan Kotlinski <kotlinski@gmail.com>
2016-07-02 12:25:05 +0200

    Tidy up

--- screen.c
+++ screen.c
@@ -20,11 +20,9 @@ THE SOFTWARE. }}} */
 
 #include <string.h>
 
-void init_screen()
-{
+void init_screen() {
     *(char*)0xd020 = 0;
     *(char*)0xd021 = 0;
-    // screen = $400, font = $2800
-    *(char*)0xd018 = 0x1a;
+    *(char*)0xd018 = 0x1a;  // screen = $400, font = $2800
     memset((char*)0x400, ' ', 40 * 25);
 }
